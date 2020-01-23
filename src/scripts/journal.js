0be47557ea7b00235c95c22b68eb78f1e2818c49
Andrew Green <green.andrew.b@gmail.com>
2020-01-23 15:51:34 -0600

    added json file to access entries w/ API

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,45 +1,45 @@
-const journalEntries = [
-    {
-        date: "01/08/2019",
-        conceptsCovered: "HTML CSS & Github",
-        longForm: "Built a webpage about the Belcourt Theatre and learned an intro to Git",
-        mood: "King ruling the castle",
-    },
-    {
-        date: "01/15/2019",
-        conceptsCovered: "Finished and presented first group project",
-        longForm: "Matt C, Tyler, Cooper, & I finished our CSS/HTML project about Ricky Gervais",
-        mood: "Duke gallivanting through the flying buttress",
-    },
-    {
-        date: "01/17/2019",
-        conceptsCovered: "Javascript functions & objects",
-        longForm: "Practiced javascript functions and objects with an example about the pope blessing Bryan and getting older",
-        mood: "Groundskeeper maintaing the manor",
-    }
-]
+// const element = (type, content, value) => {
+//     return `<${type} class="${value}">${content}</${type}>`
+// };
 
-const element = (type, content, value) => {
-    return `<${type} class="${value}">${content}</${type}>`
+// const makeJournalEntryComponent = (journalEntries) => {
+//     return `
+//     <hr class="line"></hr>
+//         <div>
+//             ${element("h2", journalEntries.date, "")}
+//             </div><div>
+//             ${element("h4", journalEntries.conceptsCovered, "")}
+//             </div><div>
+//             ${element("aside", journalEntries.longForm, "")}
+//             </div><div>
+//             ${element("h3", journalEntries.mood, "")}
+//         </div>
+//         `;
+// }
+
+const getEntries = () => {
+    const entriesUrl = "http://localhost:3000/journalEntries";
+    fetch(entriesUrl)
+        .then(resp => resp.json())
+        .then(entriesFromAPI => {
+            renderEntries(entriesFromAPI)
+        });
 };
 
-const makeJournalEntryComponent = (journalEntries) => {
-    return `
-    <hr class="line"></hr>
-        <div>
-            ${element("h2", journalEntries.date, "")}
-            </div><div>
-            ${element("h4", journalEntries.conceptsCovered, "")}
-            </div><div>
-            ${element("aside", journalEntries.longForm, "")}
-            </div><div>
-            ${element("h3", journalEntries.mood, "")}
-        </div>
-        `;
-}
+getEntries();
 
-const journalEntryContainer = document.querySelector("#container")
+// const renderEntries = (entries) => {
+//     const container = document.querySelector("#entries-container");
+//     entries.forEach(entry => {
+//         const entryAsHTML = makeJournalEntryComponent(entry)
+//         container.entryAsHTML += entryAsHTML
+//     });
+// };
 
-for (let i = 0; i < journalEntries.length; i++) {
-    journalEntryContainer.innerHTML += makeJournalEntryComponent(journalEntries[i]);
-}
+
+
+// const journalEntryContainer = document.querySelector("#entries-container")
+
+// for (let i = 0; i < journalEntries.length; i++) {
+//     journalEntryContainer.innerHTML += makeJournalEntryComponent(journalEntries[i]);
+// }
